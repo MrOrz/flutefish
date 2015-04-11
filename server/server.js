42189bd7b4d0fd8b582c501526aba8ea6c5d2e74
MrOrz <johnsonliang7@gmail.com>
2015-04-11 20:31:00 +0800

    Basic api server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    app = express();
+
+app.use(require('serve-static')('client'));
+
+// Catch-all route
+//
+app.get('*', function(req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(5000, function() {
+  console.log('Server listening at http://%s:%s',
+              server.address().address, server.address().port);
+});
