255fb17c3bf4ed169d3b215a8f85680dbd4ccebd
MrOrz <johnsonliang7@gmail.com>
2015-04-13 22:45:26 +0800

    Add IS_PRODUCTION in constant

--- common/config/constants.js
+++ common/config/constants.js
@@ -2,6 +2,8 @@ exports.PORT = process.env.PORT || 5000;
 
 exports.CHANGE = 'CHANGE';
 
+exports.IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 if (typeof window === 'undefined') {
   // Make node-fetch happy.
   exports.API_HOST = 'http://localhost:' + exports.PORT;
