a007aa65107277adc550e8c3bb73b7aea152ade1
MrOrz <johnsonliang7@gmail.com>
2015-04-14 21:55:30 +0800

    Better productStore

--- common/stores/ProductStore.js
+++ common/stores/ProductStore.js
@@ -1,27 +1,49 @@
 var createEmitter = require('../utils/createEmitter'),
-    constants = require('../config/constants');
+    constants = require('../config/constants'),
+    assign = require('object-assign');
 
 module.exports = function(context) {
-  var products = [];
+
+  // Product id --> product instance.
+  // "Single source of truth" for the products.
+  var products = {},
+
+      // Product ids for "all" list, in display order.
+      // Larger websites may have multiple lists of ids like this.
+      //
+      orderedIds = [];
 
   return createEmitter({
     _setProducts: function(newProducts) {
       if (newProducts) {
-        products = newProducts.slice(0);
+
+        // Update products and orderedIds.
+        orderedIds = newProducts.map(function(product) {
+          if (products[product.id]) {
+            assign(products[product.id], product);
+          }else {
+            products[product.id] = product;
+          }
+          return product.id;
+        });
+
         this.emit(constants.CHANGE);
       }
     },
     all: function() {
-      return products;
+      return orderedIds.map(function(id) {
+        return products[id];
+      });
     },
     get: function(id) {
-      return products.filter(function(product) {return product.id === id})[0];
+      return products[id];
     },
     dehydrate: function() {
-      return products;
+      return [products, orderedIds];
     },
     rehydrate: function(state) {
-      products = state;
+      products = state[0];
+      orderedIds = state[1];
     }
   });
 };
